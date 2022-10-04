29680e5054719ada7e7b413bd0c162da035f1327
valmirdeveloper <valmirdeveloper@gmail.com>
2022-10-04 05:09:03 -0300

    implementaçao de novo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './pagina/contato/contato.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
